bdd900739c953fb534960bd81b8dff68c6b31133
leslielow <leslie.kl91@gmail.com>
2022-10-24 18:15:56 -0300

    resuelvo promesa pendiente para mostrar los personajes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,19 +6,18 @@ const Card = (numero) => {
     const [personajeApi, setPersonajeApi] = useState({});
   
     useEffect(() => {
-        const personaje = getPersonajesApi(numero.numero);
+        const personaje = getPersonajesApi(numero.numero).then(value => {
+            // console.log("VALUE", value);
+            setPersonajeApi(value)
+        });
         
-        console.log(personaje);
-        
-        setPersonajeApi(personaje)
     }, [numero.numero] ); 
 
-    // console.log( "personajeApi",personajeApi);
+    console.log( "personajeApi",personajeApi);
 
     return (
         <div>
-            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick" />
-            {/* <img src={personajeApi.image} alt="Rick" /> */}
+            <img src={personajeApi.image} alt={personajeApi.name} />
         </div>
     )
 }
